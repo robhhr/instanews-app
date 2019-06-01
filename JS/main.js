8a42a517ed0e2f3f0befd8bd0b5d936876300413
Roberto <robhhr@outlook.com>
2019-06-01 02:51:37 -0700

    Add API for fetching news description and url

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
-const authKey = 'EXmhIZ7OAfwVNUeK9LJBZxGGg2UnGioA';
-// let sectionSelector = document.querySelector('.dropdown-list');
-// let newsBaseURL = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${authKey}`;
+let newsContent = document.querySelector('.news-content');
+let newsSelector = document.querySelector('.dropdown-section');
+let newsImage = document.querySelector('.news-image');
+let newsHeader = document.querySelector('.news-header');
 const sections = [
     'Sections ...',
     'arts',
@@ -31,61 +32,21 @@ const sections = [
     'world',
 ]
 
-for (i = 0; i < sections.length; i++) {
+for (let  i = 0; i < sections.length; i++) {
     var optns = '<option style="text-transform: capitalize">' + sections[i] + '</option>'
     $('select').append(optns);
 }
 
-// $("button").on("click", function runSections(sections, newsURL) {
-//     $.ajax({method: "GET", url: newsURL, dataType: JSON})
-//         .done(function(newsData) {
-//         const { title } = data.results[0];
-//         const { url } = data.results.multimedia[0];
-//         console.log(data);
-//     })
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-let temperatureDescription = document.querySelector('.temperature-description');
-let temperatureCondition = document.querySelector('.temperature-condition');
-let iconWeather = document.querySelector('.icons');
-
-$("button").on("click", function() {
+$('select').on('change', function() {
+    var selection = $('select').val();
     $.ajax({
-        method: "GET",
-        url: "https://api.openweathermap.org/data/2.5/weather?q=Vancouver,ca&appid=4a48e1e1428fd83889074671fbf259d9"
+        method: 'GET',
+        url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=EXmhIZ7OAfwVNUeK9LJBZxGGg2UnGioA`,
     }).done(function(data) {
-        const { description, main, icon } = data.weather[0];
-        temperatureDescription.textContent = description,
-        temperatureCondition.textContent = main,
-        iconWeather.setAttribute("src", "http://openweathermap.org/img/w/10n.png");
-    });
-}); */
+        const { url } = data.results[0].multimedia[0];
+        newsImage.textContent = url;
+        // newsHeader.setAttribute = url;
+        const { title } = data.results[0];
+        newsHeader.textContent = title;
+    })
+})
