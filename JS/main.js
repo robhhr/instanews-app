1603b65527e8459246d371afc0503d01186afe57
Roberto <robhhr@outlook.com>
2019-06-03 03:21:38 -0700

    Create first image and general styling when fetching

--- JS/main.js
+++ JS/main.js
@@ -38,7 +38,6 @@ for (let  i = 0; i < sections.length; i++) {
 }
 
 $('select').on('change', function() {
-    // $(textSection).innerText("");
     var selection = $('select').val();
     $.ajax({
         method: 'GET',
@@ -46,14 +45,12 @@ $('select').on('change', function() {
     }).done(function(data) {
         let count = 0;
         $(changeSection).html("");
-        for(let infoNews = 0; infoNews < data.results.length; infoNews++) {
-            let mainMultimedia = data.results[infoNews].multimedia[4].url;
-            let newsDescription = data.results[infoNews].title;
-            let newsItems = '<li><a id="image-content" href="' + data.results[infoNews].url +'"target="_blank"><img src="' + mainMultimedia + '"></a><p id="text-content">' + newsDescription + "</p></li>";
+            for(let infoNews = 0; infoNews < data.results.length; infoNews++) {
+                let mainMultimedia = data.results[infoNews].multimedia[4].url;
+                let newsDescription = data.results[infoNews].title;
+                let newsItems = '<li><a id="image-content" href="' + data.results[infoNews].url +'"target="_blank"><img src="' + mainMultimedia + 
+                '"></a><p id="text-content">' + newsDescription + "</p></li>";
             $(newsContent).append(newsItems);
-            if (count === 12) {
-                break;
-           }
         };
     });
 });
