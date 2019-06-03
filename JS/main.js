a59ac0c6cee816c92624c8f5a250b47004845947
Roberto <robhhr@outlook.com>
2019-06-03 07:01:10 -0700

    Add preload media for Ajax requests

--- JS/main.js
+++ JS/main.js
@@ -38,19 +38,26 @@ for (let  i = 0; i < sections.length; i++) {
 }
 
 $('select').on('change', function() {
+    $('.preload').show();
     var selection = $('select').val();
     $.ajax({
         method: 'GET',
         url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=EXmhIZ7OAfwVNUeK9LJBZxGGg2UnGioA`,
+        dataType: "json",
+        async: true,
     }).done(function(data) {
         let count = 0;
         $(changeSection).html("");
-            for(let infoNews = 0; infoNews < data.results.length; infoNews++) {
-                let mainMultimedia = data.results[infoNews].multimedia[4].url;
-                let newsDescription = data.results[infoNews].title;
-                let newsItems = '<li><a id="image-content" href="' + data.results[infoNews].url +'"target="_blank"><img src="' + mainMultimedia + 
+        // switch (count <= 12) {
+            /* case 1: */for(var infoNews = 0; infoNews < data.results.length; infoNews++) {
+                var mainMultimedia = data.results[infoNews].multimedia[4].url;
+                var newsDescription = data.results[infoNews].title;
+                var newsItems = '<li><a class="image-content" href="' + data.results[infoNews].url +'"target="_blank"><img src="' + mainMultimedia + 
                 '"></a><p id="text-content">' + newsDescription + "</p></li>";
             $(newsContent).append(newsItems);
+            $('.preload').hide();
         };
+        // break;
+    // };
     });
 });
